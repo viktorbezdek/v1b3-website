66004a2ac7c59ece7606574d220066298a571317
Viktor Bezdek <viktor.bezdek@toptal.com>
2023-01-16 10:46:45 +0100

    feat: dockerize, cleanup

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Index: NextPage = () => {
+	return (
+		<>
+			<h1>Index</h1>
+		</>
+	)
+}
+
+export default Index
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: "class", // or 'media' or 'class'
   theme: {
